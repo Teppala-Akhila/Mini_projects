87b9f402564dc1ce388404ebd1164b1186eb73a7
Teppala-Akhila <akhilaakhi96628@gmail.com>
2025-10-23 11:58:38 +0530

    Add files via upload

---
+++ movie_page/using_react/component/Body.js
@@ -0,0 +1,57 @@
+
+
+const Body=()=>{
+    return(
+        <div className="container2">
+        <div className="movie1">
+            <img src="https://play-lh.googleusercontent.com/oQxl3qEYV-aNwDUPPotxf04VR3uUeeEPk3vBTuVnOM2_MGzr6Cg2PI0AOGdL4bB70FaXXA" alt="Ninnu Kori"/>
+            <h3>Ninnu Kori</h3>
+            <p>Rating: 7.5</p>
+        </div>
+
+        <div className="movie2">
+            <img src="https://m.media-amazon.com/images/M/MV5BMGU1YzY4NjQtYTA0ZS00OTRkLWI1YWItM2FkMGFjZDk4ODFmXkEyXkFqcGc@._V1_.jpg" alt="8 Vasanthalu"/>
+            <h3>8 Vasanthalu</h3>
+            <p>Rating: 7.2</p>
+        </div>
+
+        <div className="movie3">
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/68/Bharat_Ane_Nenu_poster.jpg/250px-Bharat_Ane_Nenu_poster.jpg" alt="Bharat Ane Nenu"/>
+            <h3>Bharat Ane Nenu</h3>
+            <p>Rating: 7.4</p>
+        </div>
+
+        <div className="movie4">
+            <img src="https://m.media-amazon.com/images/M/MV5BNTU0ZjYxOWItOWViMC00YWVlLWJlMGUtZjc1YWU0NTlhY2ZhXkEyXkFqcGc@._V1_.jpg" alt="Salaar"/>
+            <h3>Salaar</h3>
+            <p>Rating: 6.9</p>
+        </div>
+
+        <div className="movie5">
+            <img src="https://m.media-amazon.com/images/M/MV5BMTBiNDVkOWQtMGZhMC00MDA0LWI4ZjUtMWE3YjRmMDc3NzBjXkEyXkFqcGc@._V1_.jpg" alt="MAD"/>
+            <h3>MAD</h3>
+            <p>Rating: 7.6</p>
+        </div>
+
+        <div className="movie6">
+            <img src="https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/major-et00097589-27-05-2022-08-31-06.jpg" alt="Major"/>
+            <h3>Major</h3>
+            <p>Rating: 6.5</p>
+        </div>
+
+        <div className="movie7">
+            <img src="https://upload.wikimedia.org/wikipedia/en/d/d7/RRR_Poster.jpg" alt="RRR"/>
+            <h3>RRR</h3>
+            <p>Rating: 7.8</p>
+        </div>
+
+        <div className="movie8">
+            <img src="https://play-lh.googleusercontent.com/bMquw55TuAmjVHGYtuplkrI9WO2lrDKpYnICF0lAlCko3IB1H4orXp-WDg0N25Rq155yz9FyGp1kJQ9ZDLo" alt="Vakeel sabb"/>
+            <h3>Vakeel Sabb</h3>
+            <p>Rating: 7.4</p>
+        </div>
+</div>
+    );
+};
+
+export default Body;
